614d8e108416af6bc88c498e2c16d4ce09500103
andresSuarez122277 <andressmol@gmail.com>
2022-04-22 07:42:06 -0500

    Se agregan entidades

--- TiendaOnline.Web/Data/ApplicationDbContext.cs
+++ TiendaOnline.Web/Data/ApplicationDbContext.cs
@@ -7,13 +7,17 @@ namespace TiendaOnline.Web.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+        public DbSet<City> Cities { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Department> Departments { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder); 
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<City>().HasIndex(t => t.Name).IsUnique();
             modelBuilder.Entity<Country>()
-                .HasIndex(t => t.Name)
-                .IsUnique();
+            .HasIndex(t => t.Name)
+            .IsUnique();
+            modelBuilder.Entity<Department>().HasIndex(t => t.Name).IsUnique();
         }
     }
 }
--- TiendaOnline.Web/Models/Country.cs
+++ TiendaOnline.Web/Models/Country.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TiendaOnline.Web.Models
 {
@@ -6,7 +8,10 @@ namespace TiendaOnline.Web.Models
     {
         public int Id { get; set; }
         [MaxLength(50, ErrorMessage = "El campo {0} debe contener al menos un caracter")]
-        [Required] 
+        [Required]
         public string Name { get; set; }
+
+        public ICollection<Department> Departments { get; set; }
+        [DisplayName("Departments Number")] public int DepartmentsNumber => Departments == null ? 0 : Departments.Count;
     }
 }
